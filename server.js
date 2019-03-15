5048e494f747615c1cca32c5f9bd52c3cbcf8329
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-03-15 10:32:16 +0530

    start mern stack

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+//Body Parser middleware
+app.use(bodyParser.json())
+
