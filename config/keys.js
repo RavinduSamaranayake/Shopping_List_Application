36a2fe1a883c672c8327cc443fb39088abc1487b
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-03-15 14:46:13 +0530

    connect to the mongo db atlas

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //MongoURI = 'mongodb+srv://kushan:kushan123@cluster0-nvdih.mongodb.net/test?retryWrites=true'
+
+    MongoURI: 'mongodb+srv://kush:kush123@cluster0-nvdih.mongodb.net/test?retryWrites=true'
+}
--- server.js
+++ server.js
@@ -5,5 +5,24 @@ const bodyParser = require('body-parser')
 const app = express()
 
 //Body Parser middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+// DB config 
+const db = require('./config/keys').MongoURI;
+
+ 
+//connect to mongo
+
+mongoose.connect(db,{ useNewUrlParser:true})
+
+    .then(() => console.log('Connected to mongodb successfully!'))
+
+    .catch(err => console.log(err));
+
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
