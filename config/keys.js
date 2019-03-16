98e7aad5bb8258e58004da209b91f766a649e1c5
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-03-17 01:56:51 +0530

    start working with front end

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    //MongoURI = 'mongodb+srv://kushan:kushan123@cluster0-nvdih.mongodb.net/test?retryWrites=true'
+ 
+    MongoURI: 'mongodb+srv://kush:kush123@cluster0-nvdih.mongodb.net/shopping?retryWrites=true' //db name shopping
+                                                                                                //Auto create the db using this string
 
-    MongoURI: 'mongodb+srv://kush:kush123@cluster0-nvdih.mongodb.net/shopping?retryWrites=true'
 }
