c33bdb2fbb7e18f91587613e99d8c2898ddc2b6a
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-03-24 06:47:07 +0530

    connect to backend

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,9 +1,14 @@
-import {GET_ITEMS,ADD_ITEM,DELETE_ITEM} from './types';
+import axios from 'axios';
+import {GET_ITEMS,ADD_ITEM,DELETE_ITEM,ITEMS_LOADING} from './types';
 
-export const getItems = () => {
-    return{
-        type: GET_ITEMS
-    };
+export const getItems = () => dispatch => {
+    dispatch(setItemsLoading());
+    axios.get('/api/items').then(res =>
+        dispatch({
+            type: GET_ITEMS,
+            payload: res.data 
+        }))
+     
 };
 
 export const deleteItem = id => {
@@ -17,5 +22,11 @@ export const addItem = item => {
     return{
         type: ADD_ITEM,
         payload: item
+    };
+};
+
+export const setItemsLoading = () => {
+    return{
+        type: ITEMS_LOADING
     };
 };
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,22 +1,22 @@
 import uuid from 'uuid';
-import {GET_ITEMS,ADD_ITEM,DELETE_ITEM} from '../actions/types';
+import {GET_ITEMS,ADD_ITEM,DELETE_ITEM,ITEMS_LOADING} from '../actions/types';
 
 const initialState = {
-    items: [
-      { id: uuid(), name: 'Eggs' },
-      { id: uuid(), name: 'Milk' },
-      { id: uuid(), name: 'Steak' },
-      { id: uuid(), name: 'Banana' }
-    ]
+    items: [],
+    loading: false
 };
 
 export default function(state = initialState, action){
     switch(action.type){
         case GET_ITEMS:
              return{
-                 ...state //return whatever is in here 
+                 ...state, //return whatever is in here 
                 //i'm doing this for a reson because when we fetch from the API we're gonna change this up a little
                 //but for now it's just gonna fetch this hard coded data
+
+                items: action.payload, //after we make the request and we get the items and we get that payload 
+                loading: false //set loading back to false
+
              }
         
         case DELETE_ITEM:
@@ -30,6 +30,13 @@ export default function(state = initialState, action){
                  ...state, //we use this '...' we can't actually mutate the state and we can't directly change it. because we have to basicly coppy of it
                  items: [action.payload , ...state.items]    
              }
+
+        case ITEMS_LOADING:
+             return{
+                 ...state, //we use this '...' we can't actually mutate the state and we can't directly change it. because we have to basicly coppy of it
+                 loading : true  
+             }
+
 
         
         default:
